c937afda692a0de529770ff7d67ba4355709c42c
sGermosenLw <sgermosen@outlook.com>
2024-05-25 11:50:04 -0400

    Adding Based Structure

--- Contactes/Contactes.Web/Models/Contact.cs
+++ Contactes/Contactes.Web/Models/Contact.cs
@@ -1,9 +1,18 @@
-﻿namespace Contactes.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Contactes.Web.Models
 {
     public class Contact
     {
+        [Key]
         public int Id { get; set; }
+
+        [MaxLength(50)]
+        [Required]
         public string Name { get; set; }
+
+        [MaxLength(50)]
+        [EmailAddress]
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
     }
