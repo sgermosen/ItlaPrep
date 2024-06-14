660b21faad977934ccb42be14e9062a56533cef9
Starling Germosen <sgermosen@outlook.com>
2024-06-14 00:10:41 -0400

    in

--- Contactes/Contactes.Domain/Base/BaseEntity.cs
+++ Contactes/Contactes.Domain/Base/BaseEntity.cs
@@ -1,9 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Contactes.Domain.Base
+﻿namespace Contactes.Domain.Base
 {
     public class BaseEntity
     {
-        [Key] public int Id { get; set; }
+        // [Key] 
+        public int Id { get; set; }
+        //public bool IsDeleted { get; set; }
+        ////   public bool Deleted { get; set; }
+        //public string CreatedBy { get; set; }
+        //public string UpdatedBy { get; set; }
+        //public string DeletedBy { get; set; }
+        //public DateTime CreatedDate { get; set; }
+        //public DateTime UpdatedDate { get; set; }
+        //public DateTime DeletedDate { get; set; }
+        //public int AuthorId { get; set; }
+
     }
 }
