ee3c57a574f93c0c501a5bc4a789db847fa72591
Starling Germosen <sgermosen@outlook.com>
2024-06-14 21:05:04 -0400

    Update Program.cs

--- MisPrimerasChambas/Mi Tercera Chamba/Mi Tercera Chamba/Program.cs
+++ MisPrimerasChambas/Mi Tercera Chamba/Mi Tercera Chamba/Program.cs
@@ -1,214 +1,313 @@
-﻿
+﻿try
+{
+    //arrays
+    //int[] numbers = { 1, 2, 3, 4, 5 };
 
-//decimal[] typedNumbers = new decimal[1];
-List<decimal> typedNumbers2 = new List<decimal>();
+    //Console.WriteLine(numbers[0]);  
+    //Console.WriteLine(numbers.Count());  
+    //Console.WriteLine(numbers.Length);
 
-//typedNumbers2.Add(1);
-//typedNumbers2.Add(2);
-//typedNumbers2.Add(3);
-//typedNumbers2.Add(4);
-//typedNumbers2.Add(5);
+    //Console.WriteLine(numbers[4]);  
+    // Console.WriteLine(numbers[6]);
 
-//Console.WriteLine(typedNumbers2[4]);
+    //int[] results = new int[5];  
+    //results[0] = 1;
+    //results[1] = 2;
+    //results[2] = 3;
+    //results[3] = 4;
+    //results[4] = 5;
+    //results = new int[6];  
 
-//typedNumbers2.RemoveAt(2);
+    //results[5] = 6;
 
-//Console.WriteLine(typedNumbers2[4]);
+    //results[5] = 0;
 
-//KEY, Value
-//Dictionary<int,decimal > valuesForDictionary = new Dictionary<int,decimal>();
-//Dictionary<int,string> names = new Dictionary<int,string>();
-//Dictionary<int,string> lastNames = new Dictionary<int,string>();
-//Dictionary<int, string> sexs = new Dictionary<int, string>();
+    //string[] strings = { "Juan", "Pedro", "Pablo", "De la Mar" };
 
-//for remove the entires 
-//names.Remove(2);
+    ////strings[0] = "";
+    //strings[1] = string.Empty;
+
+    //Console.WriteLine(results[0]);  
+
+    //Console.WriteLine(results[4]);  
+
+    //List
+
+    // List<string> fruits = new List<string> { "manzana", "banana", "cereza" };
+
+    //Console.WriteLine(fruits[2]);
+
+    //fruits.Add("durazno");  
+
+    //Console.WriteLine(fruits[3]);  
+
+    //List<int> operationHistory = new List<int>();
+    //operationHistory.Add(1);
+    // int firstOperation = operationHistory[0];
+    //Console.WriteLine(firstOperation);
+
+    //operationHistory.Add(1);
+    //operationHistory.Add(10);
+    //operationHistory.Add(15);
+    //operationHistory.Add(25);
+
+    ////operationHistory[2] = 25;
+    //Console.WriteLine(operationHistory[4]);
+
+    //operationHistory.RemoveAt(2);
+    //Console.WriteLine(operationHistory[4]);
+
+    //string [,] records = new string [3,3];
+    //records[0, 1] = "Juan";
+    //records[0, 2] = "Almonte";
+    //records[0, 3] = "M";
+
+    //records[1, 1] = "Juana";
+    //records[1, 2] = "La Cubana";
+    //records[1, 3] = "F";
+
+    //records[2, 1] = "Alberto";
+    //records[2, 2] = "Santiago";
+    //records[2, 3] = "M";
 
 
-//names.Add(1, "Juan");
-//lastNames.Add(1, "Almonte");
-//sexs.Add(1, "M");
+    ///Dictionary
+    ///
+    Dictionary<int, int> ids = new Dictionary<int, int>();
+    ids.Add(1, 1);
+    ids.Add(2, 2);
+    ids.Add(3, 15);
 
-//names.Add(2, "Juana");
-//lastNames.Add(1, "Altagracia");
-//sexs.Add(1, "F");
+    Dictionary<string, int> ages = new Dictionary<string, int>();
+    //ages["Juan"] = 25;
+    ages.Add("Juan", 25);
+    ages["Juana"] = 30;
+    ages["Alberto"] = 35;
+    //Console.WriteLine(ages["Juanz"]);
+    Console.WriteLine(ages["Juan"]);
 
-//Console.WriteLine($"Las Informaciones del estudiante son: Nombre: {names[1]}, Apellido: {lastNames[1]}");
-//Console.WriteLine($"Las Informaciones del estudiante son: Nombre: {names[2]}, Apellido: {lastNames[2]}");
 
-//for (int i = 1; i < names.Count; i++)
-//{
-//Console.WriteLine($"Las Informaciones del estudiante son: Nombre: {names[i ]}, Apellido: {lastNames[i]}");
 
-//}
-//valuesForDictionary.Add(1, 15);
-//valuesForDictionary.Add(2, 162);
-//valuesForDictionary.Add(3, 635);
-//valuesForDictionary.Add(96, 635);
+    //Dictionary<string, int> resultadosPorUsuario = new Dictionary<string, int>();
 
-//for remove the entires 
-//names.Remove(2);
+    //Leer elementos de un Diccionario
 
-//string[,] students = new string [3,3];
-//students[0, 1] = "Juan";
-//students[0, 2] = "Almonte";
-//students[0, 3] = "M";
+    //int resultadoJuan = resultadosPorUsuario["Juan"]; // Accede al resultado de Juan
+    //Console.WriteLine(resultadoJuan);
 
-//students[1, 1] = "Juana";
-//students[1, 2] = "Altagracia";
-//students[1, 3] = "F";
+    //Agregar elementos a un Diccionario
 
-Console.WriteLine(@"Bienvenido a nuestra calculadora Básica
+    //resultadosPorUsuario.Add("Ana", 42); // Agrega un resultado para Ana
+
+    //Modificar elementos de un Diccionario
+
+    //resultadosPorUsuario["Ana"] = 45; // Modifica el resultado de Ana
+
+    //Remover elementos de un Diccionario
+
+    //resultadosPorUsuario.Remove("Ana"); // Remueve el resultado de Ana Diccionarios
+
+    //Colecciones de pares clave-valor donde cada clave es única.
+
+    //Útiles para buscar valores rápidamente mediante una clave.
+
+    //Dictionary<string, int> edades = new Dictionary<string, int>();
+    //ages["Juan"] = 25;
+    //ages["Ana"] = 30;
+    //Console.WriteLine(ages["Juan"]); // Muestra la edad de Juan
+
+    //Crear un Diccionario
+
+    //Dictionary<string, int> resultadosPorUsuario = new Dictionary<string, int>();
+
+    //Leer elementos de un Diccionario
+
+    //int resultadoJuan = resultadosPorUsuario["Juan"]; // Accede al resultado de Juan
+    //Console.WriteLine(resultadoJuan);
+
+    //Agregar elementos a un Diccionario
+
+    //resultadosPorUsuario.Add("Ana", 42); // Agrega un resultado para Ana
+
+    //Modificar elementos de un Diccionario
+
+    //resultadosPorUsuario["Ana"] = 45; // Modifica el resultado de Ana
+
+    //Remover elementos de un Diccionario
+
+    //resultadosPorUsuario.Remove("Ana"); // Remueve el resultado de Ana 
+
+
+
+    decimal operationResult;
+    int typedOption = 0;
+    decimal typedNumber1;
+    //decimal typedNumber2 = 0;
+    int wantToContinueTyped = 1;
+    bool wantToContinue = true;
+    //decimal[] typedAdtionalNumers = new decimal[1];
+    List<decimal> typedAdtionalNumers = new List<decimal>();
+    Console.WriteLine(@"Bienvenido a nuestra calculadora Básica
 En este proyecto usted va a escoger una opción de la operación que desea realizar
 Y posterior a eso podrá escoger los números a los cuales les desea realizar dicha operación.");
 
+    Console.WriteLine("1. Suma \t\t 2. Resta  \t\t 3. Multiplicación  \t\t 4. Divición  \t\t 5. Cerrar");
+    Console.Write("Digite un numero del 1 al 5, para indicar la operación que usted desea realizar: ");
 
-Console.WriteLine("1. Suma \t\t 2. Resta  \t\t 3. Multiplicación  \t\t 4. Divición  \t\t 5. Cerrar");
-Console.Write("Digite un numero del 1 al 5, para indicar la operación que usted desea realizar: ");
 
-int typedOption = 0;
-try
-{
     typedOption = Convert.ToInt32(Console.ReadLine());
+    //y    o   not
+    //&&  ||   !
+    /*
+    y
+    v y v = v
+    v y f = f
+    f y v = f
+    f y f = f
+
+    o
+    v o v = v
+    v o f = v
+    f o v = v
+    f o f = f
+
+    not
+    not v = f
+    not f = v
+
+    */
 
 
-    if (typedOption > 5)
+    //if (! ((typedOption >= 6) && (typedOption <= 0)))
+    //{
+    //    Console.WriteLine("A ti te dejaron caer cuando chiquito, verdad?");
+    //}
+    // if (typedOption >= 6 || typedOption < 1)
+    //if (typedOption > 5 || typedOption <= 0)
+    if (typedOption >= 6 || typedOption <= 0)
     {
         Console.WriteLine("A ti te dejaron caer cuando chiquito, verdad?");
     }
     else
     {
 
-        decimal typedNumber1;
-        // decimal typedNumber2;
-        int userWantContinue = 1;
-        decimal operationResult;
-        //decimal typedNumber3;
-        //decimal typedNumber4;
-        //decimal typedNumber5;
-        //decimal typedNumber6;
-        //decimal typedNumber7;
-
-        //arrays 
-        //  decimal[] decimals = { 1, 2, 3, 4 };
-        //decimal[] decimals2 = new decimal [4];
-        //decimals2[0] = 1;
-        //decimals2[1] = 2;
-        //// decimals2[2] = 3;
-        //decimals2[3] = 4;
-
-        //Console.WriteLine(decimals[2]);
-        //decimals[2] = 0;
-
-
-
-        //typedNumbers2.Add(1);
-        //typedNumbers2.Add(2);
-        //typedNumbers2.Add(3);
-        //typedNumbers2.Add(4);
-        //typedNumbers2.Add(5);
-
-        //Console.WriteLine(typedNumbers2[2]);
-
-
         Console.Write("Digite el primer Número: ");
         typedNumber1 = Convert.ToDecimal(Console.ReadLine());
-
         //Console.Write("Digite el segundo Número: ");
         //typedNumber2 = Convert.ToDecimal(Console.ReadLine());
 
-        //Console.WriteLine("Desea Continuar? ");
+        //Console.WriteLine("Deseas agregar más numeros: ");
         //Console.WriteLine("1. Si, 2. No");
-        //userWantContinue = Convert.ToInt32(Console.ReadLine());
+        //// wantToContinueTyped = Convert.ToInt32(Console.ReadLine());
+        //wantToContinue = Convert.ToInt32(Console.ReadLine()) == 1;
 
-        do
-        { 
-            Console.Write("Digite un número adicional: ");
-            typedNumbers2.Add(Convert.ToDecimal(Console.ReadLine()));
+        //int count = 0;
+        //while
+        while (wantToContinue)
+        {
+            Console.Write("Digite otro Número: ");
+            decimal tempTypedValue = Convert.ToDecimal(Console.ReadLine());
+            //typedAdtionalNumers[typedAdtionalNumers.Length - 1] = tempTypedValue;
+            ////var dd =  typedAdtionalNumers.Length;
+            ////var dd2 =  typedAdtionalNumers.Count();
 
-            Console.WriteLine("Desea Continuar? ");
+            //decimal[] tempTypedNumbers = typedAdtionalNumers;
+            //typedAdtionalNumers = new decimal[typedAdtionalNumers.Length + 1];
+            ////  typedAdtionalNumers[0] = Convert.ToDecimal(Console.ReadLine()); ;
+
+            ////for (int index = tempTypedNumbers.Length; index >= 0; index--)
+            //// for (int index = tempTypedNumbers.Length; index > 1; index--) 
+            ////for (int index = 0; index < tempTypedNumbers.Length; index++)
+            //// for (int i = -1; i <= tempTypedNumbers.Length -1; i++)
+            //for (int i = 0; i <= tempTypedNumbers.Length - 1; i++)
+            //{
+            //    typedAdtionalNumers[i] = tempTypedNumbers[i];
+            //}
+            typedAdtionalNumers.Add(tempTypedValue);
+            Console.WriteLine("Deseas agregar más numeros: ");
             Console.WriteLine("1. Si, 2. No");
-            userWantContinue = Convert.ToInt32(Console.ReadLine());
+            wantToContinue = Convert.ToInt32(Console.ReadLine()) == 1;
+
         }
-        while (userWantContinue == 1);
 
-        //while (userWantContinue == 1)
+        //if (wantToContinue) //== true)
         //{
-        //    Console.Write("Digite un nuevo Número: ");
-
-        //    typedNumbers2.Add(Convert.ToDecimal(Console.ReadLine()));
-        //    //typedNumbers[typedNumbers.Length -1] = Convert.ToDecimal(Console.ReadLine());
-
-        //    //var tempValue = Convert.ToDecimal(Console.ReadLine());
-        //    //typedNumbers[typedNumbers.Length - 1] = tempValue;
-
-        //    //decimal[] typedNumbersTemp = typedNumbers;
-        //    //typedNumbers = new decimal[typedNumbers.Length + 1];
-        //    // typedNumbers = new decimal[typedNumbers.Count() + 1];
-
-        //    //for (int index = typedNumbersTemp.Length ; index > 0; index--)
-        //    //for (int index = typedNumbersTemp.Length + 1; index >= 0; index--)
-        //    // for (short index = 0; index <= typedNumbersTemp.Length -1; index++)
-        //    //for (int index = 0; index < typedNumbersTemp.Length; index++)
-        //    //{
-        //    //    typedNumbers[index] = typedNumbersTemp[index];
-        //    //}
-
-        //    Console.WriteLine("Desea Continuar? ");
+        //    Console.WriteLine("Deseas agregar más numeros: ");
         //    Console.WriteLine("1. Si, 2. No");
-        //    userWantContinue = Convert.ToInt32(Console.ReadLine());
+        //    //wantToContinue = true;
         //}
-        //Console.WriteLine(typedNumbers[10]);
+        //else
+        //{
+        //    //wantToContinue = false;
+        //}
+        //if (!(wantToContinue)) // == false)
+        //{
 
-        //Console.WriteLine(typedNumbers2[10]);
 
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Deseas agregar más numeros: ");
+        //    Console.WriteLine("1. Si, 2. No");
+        //    //wantToContinue = false;
+        //}
+
+        //if (wantToContinue == true) {
+        //    Console.WriteLine("Deseas agregar más numeros: ");
+        //    Console.WriteLine("1. Si, 2. No");
+
+
+        //}
         operationResult = typedNumber1;
+
         switch (typedOption)
         {
             case 1:
                 {
-                    //operationResult = typedNumber1; // + typedNumber2;
-                    //for (int index = 0; index < typedNumbers.Length; index++)
+                    // for (int i = 0; i <= typedAdtionalNumers.Length - 1; i++)
+                    //for (int i = 0; i <= typedAdtionalNumers.Count() - 1; i++)
+                    //for (int i = 0; i <= typedAdtionalNumers.Count - 1; i++)
                     //{
-                    //    operationResult += typedNumbers[index];
-                    //   // operationResult = operationResult + typedNumbers[index];
-                    //}
-                    for (int index = 0; index < typedNumbers2.Count; index++)
-                    {
-                        operationResult += typedNumbers2[index];
-                    }
+                    //    //  operationResult = operationResult + typedAdtionalNumers[i];
+                    //    operationResult += typedAdtionalNumers[i];
+                    //};
 
+                    foreach (decimal item in typedAdtionalNumers)
+                    {
+                        operationResult += item;
+                    }
                 }
+
                 break;
             case 2:
-               // operationResult = typedNumber1 - typedNumber2;
-
                 {
-                   // operationResult = typedNumber1;  
-                    for (int index = 0; index < typedNumbers2.Count; index++)
+                    //  operationResult = typedNumber1; 
+                    foreach (decimal item in typedAdtionalNumers)
                     {
-                        operationResult -= typedNumbers2[index];
+                        operationResult -= item;
                     }
-
                 }
+
                 break;
             case 3:
-                { 
-                    for (int index = 0; index < typedNumbers2.Count; index++)
-                    {
-                        operationResult *= typedNumbers2[index];
-                    } 
-                }
-                //operationResult = typedNumber1 * typedNumber2;
-                break;
-            case 4:
-                //operationResult = typedNumber1 / typedNumber2;
                 {
-                    for (int index = 0; index < typedNumbers2.Count; index++)
+                    // operationResult = typedNumber1; 
+                    foreach (decimal item in typedAdtionalNumers)
                     {
-                        operationResult = operationResult / typedNumbers2[index];
+                        operationResult *= item;
                     }
                 }
+
+                break;
+            case 4:
+                {
+                    // operationResult = typedNumber1;  
+                    foreach (decimal item in typedAdtionalNumers)
+                    {
+                        operationResult /= item;
+                    }
+                }
+
                 break;
             default:
                 operationResult = 0;
@@ -218,15 +317,20 @@ try
         Console.WriteLine($"Terminamos...");
     }
 }
-catch (IndexOutOfRangeException ex)
+catch (IndexOutOfRangeException)
 {
-    Console.WriteLine("El programador inepto intentó acceder a un elemento de algun arreglo, que no tiene la dimencion especificada");
+    Console.WriteLine("En algun arreglo estas intentando acceder a un indice no existe");
 }
-catch (ArgumentOutOfRangeException ex)
+catch (ArgumentOutOfRangeException)
 {
-    Console.WriteLine("El programador inepto intentó acceder a un elemento de algun lista, que no tiene la dimencion especificada");
+    Console.WriteLine("En alguna lista estas intentando acceder a un elemento no existe");
 }
-catch (DivideByZeroException ex)
+catch (KeyNotFoundException)
+{
+    Console.WriteLine("En algun diccionario estas intentando acceder a una llave que no existe");
+
+}
+catch (DivideByZeroException)
 {
     Console.WriteLine("No se puede dividir entre cero maldito animal!!!");
 }
